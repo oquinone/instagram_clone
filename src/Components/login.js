d60e1058ce92bb5e1c67f98a042f8be3c1c5a41b
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-05-21 17:09:03 -0700

    Moved signup sections to be its own component

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Form, Button } from 'react-bootstrap';
 import Logo from '../images/logo.png';
+import { Signup } from './signup';
 
 import '../styling/login.scss';
 import '../styling/globals.scss';
@@ -8,39 +9,12 @@ import '../styling/globals.scss';
 export const Login = () => {
     const [isLogin, setIsLogin] = useState(true);
 
-
-    if(isLogin){
-        return (
-            <div className="login-container">
-                <section className="flex-c login">
-                    <div className="login-header">
-                        <img src={Logo} alt={Logo} />
-                    </div>
-                    <div className="login-form">
-                        <Form>
-                            <Form.Group controlId="formGroupEmail">
-                                {/* <Form.Label>Email</Form.Label> */}
-                                <Form.Control type="email" placeholder="Email" />
-                            </Form.Group>
-                            <Form.Group controlId="formGroupPassword">
-                                {/* <Form.Label>Password</Form.Label> */}
-                                <Form.Control type="password" placeholder="Password" />
-                            </Form.Group>
-                        </Form> 
-                    </div>
-                    <div className="login-btn">
-                        <Button variant="primary">Login</Button>
-                    </div>
-                    <div className="login-signup">
-                        <span>
-                            <p>Don't Have An Accountm </p>
-                            <Button onClick = {() => setIsLogin(false)}> Sign up</Button>
-                        </span>
-                    </div>
-                </section>
-            </div>
-        );
+    const hasAccount = () => {
+        setIsLogin(true);
     }
+
+    if(!isLogin) return <Signup  login={hasAccount}/> 
+
     return (
         <div className="login-container">
             <section className="flex-c login">
@@ -49,31 +23,24 @@ export const Login = () => {
                 </div>
                 <div className="login-form">
                     <Form>
-                        <Form.Group controlId="formGroupName">
-                            <Form.Control type="test" placeholder="Username" />
-                        </Form.Group>
                         <Form.Group controlId="formGroupEmail">
                             <Form.Control type="email" placeholder="Email" />
                         </Form.Group>
                         <Form.Group controlId="formGroupPassword">
                             <Form.Control type="password" placeholder="Password" />
                         </Form.Group>
-                        <Form.Group controlId="formGroupPassword">
-                            <Form.Control type="password" placeholder="Enter Password" />
-                        </Form.Group>
                     </Form> 
                 </div>
                 <div className="login-btn">
-                    <Button variant="success">Sign Up</Button>
+                    <Button variant="primary">Login</Button>
                 </div>
                 <div className="login-signup">
                     <span>
-                        <p>Already Have An Account? </p>
-                        <h1 onClick = {() => setIsLogin(true)}> Login</h1>
+                        <p>Don't Have An Accountm </p>
+                        <Button onClick = {() => setIsLogin(false)}> Sign up</Button>
                     </span>
                 </div>
             </section>
         </div>
     );
-    
 }
