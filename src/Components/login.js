052697edd4a1c5e89571be49cb944e6e0dd0f976
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-06-08 23:32:11 -0700

    Check to see if new user was added to the db then display an alert message to login

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,40 +1,57 @@
 import React, { useState } from 'react'
-import { Form, Button } from 'react-bootstrap';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Logo from '../images/logo.png';
-import { Signup } from './signup';
+import { useSelector } from 'react-redux';
 
 import '../styling/login.scss';
 import '../styling/globals.scss';
 
 export const Login = () => {
-    const [isLogin, setIsLogin] = useState(true);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const { signUpSuccessfull } = useSelector((state) => state.signUpStore);
 
-    const hasAccount = () => { setIsLogin(true); }
-
-    if(!isLogin) return <Signup  login={hasAccount}/> 
+    const login = () => {
+        const url = "http://localhost:5000/login";
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body:JSON.stringify({
+                email: email,
+                password: password
+            })
+        }
+        fetch(url, options)
+        .then(res => res.json())
+        .then(data => console.log(data));
+    }
 
     return (
         <div className="login-container">
             <section className="flex-c login">
                 <div className="login-header">
                     <img src={Logo} alt={Logo} />
+                    {signUpSuccessfull ? <Alert variant="success"> Use Credentials to Login</Alert> : null}
                 </div>
                 <div className="login-form">
                     <Form>
                         <Form.Group controlId="formGroupEmail">
-                            <Form.Control type="email" placeholder="Email" />
+                            <Form.Control type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
                         </Form.Group>
                         <Form.Group controlId="formGroupPassword">
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
                         </Form.Group>
                     </Form> 
                 </div>
                 <div className="login-btn">
-                    <Button variant="primary">Login</Button>
+                    <Button variant="primary" onClick={() => login()}>Login</Button>
                 </div>
                 <div className="login-signup">
                     <span>
-                        <p>Don't Have An Account?  <em onClick = {() => setIsLogin(false)}> Sign up</em></p>
+                        <p>Don't Have An Account?  <Link to="/signup"><em> Sign up</em></Link></p>
                     </span>
                 </div>
             </section>
