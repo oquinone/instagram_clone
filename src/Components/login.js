d5efefd6293b08899ae79995b73f8b782ae1f85a
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-06-10 16:57:26 -0700

    Use Redux to hold user info if login is sucessful

--- src/Components/login.js
+++ src/Components/login.js
@@ -1,34 +1,35 @@
 import React, { useState } from 'react'
 import { Form, Button, Alert } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import Logo from '../images/logo.png';
-import { useSelector } from 'react-redux';
 
+import { useSelector, useDispatch } from 'react-redux';
+import { loginReq } from '../fetch/login';
+import { setInfo, setUsername } from '../redux/signup';
 import '../styling/login.scss';
 import '../styling/globals.scss';
 
 export const Login = () => {
+    const dispatch = useDispatch();
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
     const { signUpSuccessfull } = useSelector((state) => state.signUpStore);
 
-    const login = () => {
-        const url = "http://localhost:5000/login";
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-              },
-            body:JSON.stringify({
-                email: email,
-                password: password
-            })
+    const submit = () => {
+        const data = loginReq(email, password);
+        if(data['success']){
+            dispatch(setInfo(data['_info']));
+            dispatch(setUsername(data['username']));
+            setIsLoggedIn(true);
         }
-        fetch(url, options)
-        .then(res => res.json())
-        .then(data => console.log(data));
     }
 
+    if(isLoggedIn){
+        return(
+            <Redirect to="/profile" />
+        );
+    }
     return (
         <div className="login-container">
             <section className="flex-c login">
@@ -47,7 +48,7 @@ export const Login = () => {
                     </Form> 
                 </div>
                 <div className="login-btn">
-                    <Button variant="primary" onClick={() => login()}>Login</Button>
+                    <Button variant="primary" onClick={() => submit()}>Login</Button>
                 </div>
                 <div className="login-signup">
                     <span>
