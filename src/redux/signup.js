230644ef34aaeee40c87c26bc72833d91f2c0635
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-06-10 16:58:13 -0700

    Added variables to hold user db _id && username

--- src/redux/signup.js
+++ src/redux/signup.js
@@ -3,14 +3,22 @@ import { createSlice } from '@reduxjs/toolkit';
 export const loginSlice = createSlice({
     name: 'loginAlert',
     initialState:{
-        signUpSuccessfull: false
+        signUpSuccessfull: false,
+        _info: "",
+        username: ""
     },
     reducers:{
         setSignUpSuccess: (state, val) => {
             state.signUpSuccessfull = val;
+        },
+        setInfo: (state, id) => {
+            state._info = id;
+        },
+        setUsername: (state, name) => {
+            state.username = name;
         }
     }
 });
 
-export const { setSignUpSuccess } = loginSlice.actions;
+export const { setSignUpSuccess, setInfo, setUsername } = loginSlice.actions;
 export default loginSlice.reducer
