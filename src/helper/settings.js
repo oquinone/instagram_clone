aa0b9b4ebb2c3eda43b0bf62a7f0f198410fb790
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-06-17 09:12:33 -0700

    Helper folder holds any javascript functions that acn be needed for components

---
+++ src/helper/settings.js
@@ -0,0 +1,22 @@
+export const change = async (profilePic, file) => {
+    if(profilePic !== undefined){
+        
+        return (await convertBase64(file));
+    }
+    return null;
+}
+
+const convertBase64 = (file) => {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.readAsDataURL(file);
+
+        fileReader.onload = () => {
+            resolve(fileReader.result);
+        };
+
+        fileReader.onerror = (error) => {
+            reject(error);
+        };
+    })
+}
