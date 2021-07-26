e6900651c2ef410ca8b136731d13ef169b48698f
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-07-26 14:01:26 -0700

    Changed BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom' 
+import { HashRouter as Router, Route, Switch } from 'react-router-dom' 
 import { Spinner } from 'react-bootstrap';
 
 import { Likes } from './Components/likes';
