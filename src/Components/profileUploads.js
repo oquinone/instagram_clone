0bbed58ca8831d81475fca8f11a187fba4c9cbbc
omar quinones <omarquinones@omars-mbp.attlocal.net>
2021-08-05 17:53:50 -0700

    Added functionality to know what image the user is clicking from array

--- src/Components/profileUploads.js
+++ src/Components/profileUploads.js
@@ -1,12 +1,22 @@
 import React from 'react'
+// import { Button } from 'react-bootstrap';
+import { useDispatch } from 'react-redux';
+
+import { setSelectedImage } from '../redux/profile';
 
 export const ProfileUploads = ({images}) => {
+    const dispatch = useDispatch();
+    // const { selectedImage } = useSelector((state) => state.profile);
+
     return(
         <section className="profile-uploads">
                 <hr/>
                 <div className="profile-uploads-flex">
                     {images !== undefined ? 
-                    (images.reverse().map((data, index) => <div key={index}> <img src={data} alt={data}/></div>))
+                    (images.map((data, index) => 
+                    <div key={index} onClick={() => dispatch(setSelectedImage(index))}> 
+                        <img src={data} alt={data}/>
+                    </div>))
                     : 
                     null}
                 </div>
