093d694e6c7a28d5aa2fdfbd352d3253f8674b22
Omar <account1@fulls-mbp.attlocal.net>
2024-05-15 02:15:26 -0700

    updated state management to be one file

---
+++ src/zucstand/store.js
@@ -0,0 +1,52 @@
+import { create } from "zustand";
+
+// basic info store
+export const useInfoStore = create((set) => ({
+  id: 0,
+  username: "",
+  bio: "",
+  profileImage: "",
+  uploadedImages: [],
+
+  setBio: (newBio) => set((state) => ({ bio: newBio })),
+  setUserName: (newUsername) => set((state) => ({ username: newUsername })),
+  setInfo: ({ id, username, bio }) =>
+    set((state) => ({
+      id: id,
+      username: username,
+      bio: bio,
+    })),
+  setData: ({ username, bio }) =>
+    set((state) => ({
+      username: username,
+      bio: bio,
+    })),
+  setProfileImage: (newImage) => set((state) => ({ profileImage: newImage })),
+  setId: (newId) => set((state) => ({ id: newId })),
+  setUploadedImages: (images) => set((state) => ({ uploadedImages: images })),
+}));
+
+// image upload store
+export const useImageUploadState = create((set) => ({
+  image: "",
+  imageName: "",
+  uploadedImage: null,
+  uploaded: false,
+  profileImage: "",
+
+  setImage: (newImage) => set((state) => ({ image: newImage })),
+  setImageName: (newImageName) => set((state) => ({ imageName: newImageName })),
+  setUploadedImage: (newUploadImage) =>
+    set((state) => ({ uploadedImage: newUploadImage })),
+  isUploaded: (isUploaded) => set((state) => ({ uploaded: isUploaded })),
+  setProfileImage: (newProfImage) =>
+    set((state) => ({ profileImage: newProfImage })),
+}));
+
+// sign up store
+export const useSignUpStore = create((set) => ({
+  userSignUp: false,
+
+  setUserSignUp: (newImage) => set((state) => ({ image: newImage })),
+  resetSignUpStore: () => set((state) => ({ userSignUp: false })),
+}));
