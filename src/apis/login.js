580ee890ea46ee68fb370f7bf78577b3ba04d7cd
Omar <account1@omars-mbp.attlocal.net>
2024-06-03 00:24:03 -0700

    Changed some endpoints to be http

--- src/apis/login.js
+++ src/apis/login.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const loginAPI = async (data) => {
   try {
     const response = await axios.post(
-      "https://instacloneapi-env.eba-dsivkvr7.us-east-2.elasticbeanstalk.com/api/auth/authenticate",
+      "http://instacloneapi-env.eba-dsivkvr7.us-east-2.elasticbeanstalk.com/api/auth/authenticate",
       {
         ...data,
       }
@@ -19,7 +19,7 @@ export const loginAPI = async (data) => {
 export const getLoginDataAPI = async ({ email, token }) => {
   try {
     const response = await axios.get(
-      `https://instacloneapi-env.eba-dsivkvr7.us-east-2.elasticbeanstalk.com/info/findUser`,
+      `http://instacloneapi-env.eba-dsivkvr7.us-east-2.elasticbeanstalk.com/info/findUser`,
       {
         params: {
           email,
--- src/apis/signup.js
+++ src/apis/signup.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const signUpUserAPI = async (data) => {
   try {
     const response = await axios.post(
-      "https://instacloneapi-env.eba-dsivkvr7.us-east-2.elasticbeanstalk.com/api/auth/signup",
+      "http://instacloneapi-env.eba-dsivkvr7.us-east-2.elasticbeanstalk.com/api/auth/signup",
       {
         ...data,
       }
